409b6fbb6bcd6e2d8d03492d9c4b6cb9f5223788
MateusTMagalhaes <159064680+matt-teles@users.noreply.github.com>
2026-08-12 19:22:13 -0300

    feat: add ESP32 TCP client for Ethernet validation

--- firmware/esp32/src/main.cpp
+++ firmware/esp32/src/main.cpp
@@ -1,18 +1,67 @@
 #include <Arduino.h>
+#include <ETH.h>
 
-// put function declarations here:
-int myFunction(int, int);
+IPAddress serverIP(192, 168, 15, 8);
+const uint16_t serverPort = 5005;
+int messageCount = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+WiFiClient client;
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(2000);
+
+  Serial.println("Starting Ethernet...");
+
+  ETH.begin(
+      1,  // PHY address
+      16, // PHY power
+      23, // MDC
+      18, // MDIO
+      ETH_PHY_LAN8720,
+      ETH_CLOCK_GPIO0_IN);
+
+  Serial.print("Waiting for IP");
+
+  while (ETH.localIP() == IPAddress(0, 0, 0, 0))
+  {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println();
+  Serial.print("IP address: ");
+  Serial.println(ETH.localIP());
+
+  Serial.print("Connecting to server...");
+
+  while (!client.connect(serverIP, serverPort))
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println();
+  Serial.println("TCP connected.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  if (messageCount >= 10)
+  {
+    Serial.println("Closing TCP connection...");
+    client.stop();
+    Serial.println("Connection closed.");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    /* Stop all other commands until reset*/
+    while (true)
+    {
+      delay(1000);
+    }
+  }
+
+  client.println("hello");
+  messageCount++;
+  delay(1000);
 }
