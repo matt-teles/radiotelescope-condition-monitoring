4582487a275257567fcd0d55e8b5255fc50f3502
MateusTMagalhaes <159064680+matt-teles@users.noreply.github.com>
2026-08-13 13:15:50 -0300

    feat: TCP server now prints sample rate every 1s

--- backend/TCP_server.py
+++ backend/TCP_server.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 HOST = "0.0.0.0"
 PORT = 5005
@@ -15,10 +16,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     with conn:
         print("connected:", addr)
 
+        sample_count = 0
+        last_report = time.monotonic()
+
         while True:
             data = conn.recv(1024)
 
             if not data:
                 break
 
-            print(data.decode(), end="")
+            sample_count += data.count(b"\n")
+            now = time.monotonic()
+
+            if now - last_report >= 1.0:
+                print(f"Received: {sample_count} samples/s")
+
+                sample_count = 0
+                last_report = now
